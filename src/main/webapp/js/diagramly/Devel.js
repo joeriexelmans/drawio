676a186940cbb30661d07405d2c32ecc9b744aae
Joeri Exelmans <joeri.exelmans@gmail.com>
2020-11-24 18:23:31 +0100

    Disable CSP under all circumstances

--- src/main/webapp/js/diagramly/Devel.js
+++ src/main/webapp/js/diagramly/Devel.js
@@ -61,7 +61,7 @@ if (!mxIsElectron && location.protocol !== 'http:')
 			replace(/%style-src%/g, 'https://devhost.jgraph.com').
 			replace(/%frame-src%/g, '').
 			replace(/  /g, ' ');
-		mxmeta(null, devCsp, 'Content-Security-Policy');
+		//mxmeta(null, devCsp, 'Content-Security-Policy');
 
 		if (urlParams['print-csp'] == '1')
 		{
