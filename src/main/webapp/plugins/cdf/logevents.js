9f8113ffa0710fe6ae53103742c61c716093a466
Joeri Exelmans <joeri.exelmans@gmail.com>
2020-12-10 19:36:22 +0100

    Implement screenshare with 'live preview' while dragging shape

--- src/main/webapp/plugins/cdf/logevents.js
+++ src/main/webapp/plugins/cdf/logevents.js
@@ -1,13 +1,40 @@
 Draw.loadPlugin(function(ui) {
-  // console.log("Plugin 'logevents' will log the following events:")
-  const events = Object.entries(mxEvent)
-    .filter(([key,val]) => typeof val === 'string')
-    .filter(([key,val]) => val !== mxEvent.FIRE_MOUSE_EVENT) // ignore mouse events (too many of them :)
-    // .map(([key,val]) => {console.log("  mxEvent."+key); return [key,val];})
+  const graph = ui.editor.graph;
 
-  events.forEach(([key,val]) => {
-    ui.editor.graph.model.addListener(val, (source, eventObj) => {
-      console.log("mxEvent." + key, eventObj);
-    })
-  })
+  // console.log("Plugin 'logevents' will log the following events:")
+  // const events = Object.entries(mxEvent)
+  //   .filter(([key,val]) => typeof val === 'string') // only 'string' entries of mxEvent are events
+  //   // .filter(([key,val]) => val !== mxEvent.FIRE_MOUSE_EVENT) // ignore mouse events (too many of them :)
+  //   // .map(([key,val]) => {console.log("  mxEvent."+key); return [key,val];})
+
+  const new_handler = (label) => {
+    return (source, eventObj) => {
+      if (eventObj.name == mxEvent.FIRE_MOUSE_EVENT)
+        return; // only print consumed mouse events - otherwise there are too many!
+
+      console.log(label + " emit ", eventObj);
+    };
+  }
+
+  // ui.editor.graph.model.addListener(null, new_handler("mxGraphModel"));
+  // ui.editor.graph.addListener(null, new_handler("mxGraph"));
+
+
+  // const oldStart = graph.graphHandler.start;
+  // graph.graphHandler.start = function(cell, x, y, cells) {
+  //   console.log("mxGraphHandler.start", cell, x, y, cells);
+  //   oldStart.apply(this, arguments);
+  // }
+
+  // const oldReset = graph.graphHandler.reset;
+  // graph.graphHandler.reset = function() {
+  //   console.log("mxGraphHandler.reset")
+  //   oldReset.apply(this, arguments);
+  // }
+
+  // const oldUpdateLivePreview = graph.graphHandler.updateLivePreview;
+  // graph.graphHandler.updateLivePreview = function(dx, dy) {
+  //   console.log("mxGraphHandler.updateLivePreview", dx, dy)
+  //   oldUpdateLivePreview.apply(this, arguments)
+  // }
 })
