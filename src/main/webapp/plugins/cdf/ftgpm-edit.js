af72c37b4c33075a957e1f310c1c0741656a5672
Joeri Exelmans <joeri.exelmans@gmail.com>
2021-06-03 09:53:03 +0200

    Embed FTG+PM library in ftgpm-edit plugin.

--- src/main/webapp/plugins/cdf/ftgpm-edit.js
+++ src/main/webapp/plugins/cdf/ftgpm-edit.js
@@ -1,111 +1,108 @@
-const styles = {
-  "control_flow": "endArrow=classic;html=1;strokeWidth=2;",
+{ // don't pollute global namespace
+  const libraryData = '<mxlibrary>[{"xml":"dVJNc4MgEP013Al0nOk1ps2pl/TQYwd1o7QoFtdo/n0XxI+kDTOO7Hv7+Vgm03o8OtVWb7YAw+QLk6mzFqdbPaZgDBNcF0wemBCcPiZeH7C7wPJWOWjwnwCbfUGO5GFU5osdApkY8t2fbeOZ3BrrApP89L6NPROSh7OFkjL+Q6y+B7IZUD3aWiEUC+MWKkd90XidGeoyu89D2J/chE29znAYuT5ZA9NEoebnml5sFBLevBWvw+sc6WzfFOBdd5R5qDTCe6tyzw70RoRVWJtId+jsN3zoAitChJdQG5Mu+skzV5KrKO0WD2dp/QIOYXz4gLtNx0ewNaCjmfgQ6xL7PL0xr0CXVYx6ipjqJrtcItdtoEsUYTbjfszmuofB9WZNfwE=","w":90,"h":40,"aspect":"fixed","title":"PM: Automated Activity"},{"xml":"dZLBcoMgEIafhjvBjjO9xrQ59ZIeeuygboQWxCJG8/ZdENSkDTOO7PfvLsuyJCv0dLS8E2+mBkWyF5IV1hg37/RUgFKEUVmT7EAYo/gR9vpA3QWVdtxC6/4JMOUXVA49FC/9YYcg5gp992fTeqUyytig5D+DL2NPWEbD2qK8if8QK+9BmYDm7cDVgm3ivHLyIt01KVhieZ8E2Z/EyOZCEw731SejYL4OHvi5Jmeb5jBv3vatd9cUZ83Q1uBdd5h3FNLBe8crr474PMiE0yrKvbPmGz5k7QQSFrtXLK3LzmF5LpXa8CKspfALWAfTw7fbbSo+gtHgLN6JjvFcVJ/n56UCZCNi1FNkvJ/tZolcBwE3sQnJjKORzHUEg+vNhP4C","w":90,"h":40,"aspect":"fixed","title":"PM: Manual Activity"},{"xml":"dVPRjqowEP0aknvfgMqijwKKq7KyatTlrUCXFgpl2yro129BvavJXRKSmXPOMJOZgwbcsvU5rHHAUkQ1MNGAyxmT16hsXUSpZuok1YCnmaauXs2c/sIaPavXkKNK/qeAxTlKpFJQGHfNvJ7sS8o1o+iKkIpIAumVuH+61z13FfJ8LxEY1l0oJKoSQv/ku6X0WJB5swaq7vnbIBM7aWum4zd5Eble7DFQWcPVIjiuceCGh69Rga1gR+1DuPWctygAc7GZDm08C/22sQ/yne3O5moy3/LKaIzAH2+jbKL7deNP53uAS9VmXVgwOY/5Oo38xWbLd9a7Ql/xKgzRizuC7W4ElroUJjYzy3jf83kTzz4ukyifOvQ8iT4u+WA8Px9GOTFAuHidtGZ4Khoj0+vTMnRLfWQealNQ+pKeZvLoxmG8T76CJK+HajgrE2oDfzXgNJhItKlh0q2kUcdVGJZlt3BDhVDU3RWA90lapNbrqO2lrFGArpKMQiFucX8mByZFxtmxSl1GGVdUxSrUlUnOCrQnqcQKVNdxPgmld5FmgpHt6bbd4aySG3LpxjEGt/xBp/dPNxklWaWwRNkHKdLhSJALjPszdwLOJJQP+dUjJ8Qlan+1oPFgHR+xEkl+VpLmNrhiwdWlOkYkw/IZg+KaZ/8qf/ysgpsb7+nN4ff050/qpU8/2jc=","w":30,"h":30,"aspect":"fixed","title":"PM: Initial"},{"xml":"dVNrc6IwFP01zOx+A2LX8lEe1kfVWqzWfgsQSTAPNkRBf30vqFs7s2UmM/eec08Scs+1UCCaJ41LOlMZ4RaKLBRopcwlEk1AOLdcm2UWCi3XtWFZ7vAH1ulYu8SaSPMfgUoKkhqo4DhpDws7spOIV8XJBdkxifkFvm3cVX0/szKnm6CiuGzDyhCZMv7LrF9MqMb5cGRjOJstKi8xH8dtSh8m9mw0D2xTe75wiwV6KosUthguwuMuDXqFMEzhImeHKLSLZTGdAUf6XnbYvpvDaNbgYFCUaZNM0Ot8Y7k+zQYz/+9YJenI3bzTAUDk7QQilCdzsU6mb73hZq6iKh/T1OkPVs7Uyb0MKXpSyF3jPHoIl1SAbFWQXEkhymIJ8mh2XvrRVo+aI+8fgY7i8WItJ2wUPbPtYbKKXenu3xfbj0CqPZJQ4T2CrjhOC4F6NIu91dqMz8s/E8dEWeM/rHS6g4J4iV4S8taQeLHvPT4+i9cUXu63hfyaMkPiEqftU9ZgCcCoEW2bHAhxVba9Q+GONQTa4sOrZ6oGwIYk57iqrnHXXB+n+1yrg8wCxZUGSipJWpnRak82LDMUQOiqv2Oc34osF3n90O73W1xJE7Nzex2nd83v6uzua2/GWS4BS8F0RHcAV/VAa1VX18trZbDBSWeYVqJJxc53OcnYPX0x35FoQ5ofne3cefKJKEGMhsbb9fXPgEUX89uUsJya7xiuLnn+T/k1JhBcbX5Lr4NzS78GtCv9Nr+f","w":30,"h":30,"aspect":"fixed","title":"PM: Final"},{"xml":"dVPbbqMwEP0apN03A7nxyCUhaci9hG5eVgac2MTYxLgJ4etrINlupRYJaebMDB7OOdZMN698AQu84CmimjnWTFdwLrsor1xEqWYAkmqmpxkGUK9mTH6o6m0VFFAgJr8Z4HGGEqk6KIybw7y22I7kW05Rhxy5OP/NOGFd6fnxtvPruaW8P4dKDIsmLCViCaG/6D6IvXJ28tYGvJL12KnUFkt9bdmYDxbAftsM0lGQusO6PN9ebWIy62WX2JfZPEM3kCyhbdhTNh/FO3YNNMMJDgiDu9j19/vBrGdF42pOKj02Cz7wMPOXFjnk+9XdibcLJurxhoYBOxeFM9F7fApeT7hm73gahZdMH14DPAgjw18RPwUrScvDRIrJngOIwjqfOcnCR/Uler/es37sWmxZzf7Ad7VEKGL1F+vD2hmOFNdOpjJ/B423l+Uoqv3zdhPm0/5ms+0pJn5rpnPDRKJdAZOGmpuSWWFY5g31ugpLKfgZRSSVWCGKXedIKHU55aJl1QTt0+Ccye9wSMmJKSxRgiPRAGXRSGx6R1IhpZzTiXhFQqLqR5fo/2nrI54jKe6q5fbYTFWtzkgAI3LCj6mHuQAsu/z0b/LTcip42OWZPkz4TD/N3rZ+uQsf","w":90,"h":20,"aspect":"fixed","title":"PM: Fork/Join"},{"xml":"dVPbcqMwDP0aZnbfuGRD8hguaVNCIRfSJm8GHOwEMGubAPn6CpJs25ktM56Rjo5sIR0phl20TxxVxGcpzhXDVQybMyZvVtHaOM8VXaWpYjiKrqtwFH3+Q1QbomqFOC7lfxJYfMKJBEaO4v4xZwgOKcWa5fiGpDihgrLyFnncPRC/Pytk98gRBFW9KSQuE5r/4odX6TA/cxaHsDEU3XLNmb2EOyyTuDZeR6vXelzrz8w/XA5m7HpeFJrqBdGCMJWeShrMInUl6Ivv7C+Flmzr5eLPS7M7nvz1fA8/NJX0rB6WTZSkrme/7LrlnhWb8WRRdsY2DHWbZF0k8oWdW2fit/Zab5nJgtG6a0fonXK820nL0mZ2UUNRy2m8WZ2Z68ZRCJenm92MBF4dv8ba0ziaoJoZozauTNFsjbNx8orpGGUaf9ZNFE8qj61QdzxCIheJETZVaFpSzIPOmUCz56Kz9/tjAM/8LWLv7Z1Og2vYzYOkiISZeNDJ+ban+WQCHfytGFZDqMSbCiV9SxtQB2BEFv3ENDCRqPoxGs6RthjGY0H3U9YAoIKT5UiIuz3M2ULJOeOsLlOb5YxDqGQl7tMkZ2f8RlNJAITpWkea5w+SohtT01FNs8dZKTf02pejje7+F546fH1lOc1KwBLQH4agxbGgVxQPMukJnEkkv/g3jV0wl7j9UcPaF+k9YVZgyTugNPfCITq6yVwlmGZEfseQuPnZv8zPhQDjruaHe1+Rh/u5igP126Z+AA==","w":40,"h":40,"aspect":"fixed","title":"PM: Decision/Merge"},{"xml":"dVJNb4MwDP01OW4CAm13LGztaZf2sOOUggvZAkGJ+7VfP4eElkptJKT42e897Jjxoj2vjeibT12BYvyD8cJojf7WngtQiiWRrBh/Z0kS0ceS1ZNsPGSjXhjo8AFB736gRKpQYufMXMH20IMhaFmiPEq8BIl2oxX4CusqvsVdfnRMXHj/MxYvI/PUSIRtL0oXn6hLxvMGW+cc09Wi0b/wJStsCCGdfC+VKrTSZuDzFZ2iILzUHQrZgQnMIxiUpVBLJeuOMNROek9V2+DOQzxRi4ZDuAiskqbkJHMD5cFYeYQNWPnn6K7Md+qs4Px0vvFkAGvQLaChEUWBkL5m6Tyd8yxaLGbpLI3hJeZe5BTadkNc+FeKGpB1Mwq/BVBYD9RX9avhhl5TdDU1fHWczQPt4uMxnNjF2QM7nt27CUWj6QRCrg9dZadbRJdJrzdoeP8xDKs2hreV9uzpxv8D","w":280,"h":190,"aspect":"fixed","title":"PM: Super Activity"},{"xml":"3VfbcqM4EP0aV+0+zBYgMObRYHwNMb7Et5ctATIIAyIgDM7XrzA48S2eyUOmplZVqUhH6m6p++hYNIAWFr0Exp5BHBQ0gN4AWkIIrXphoaEgaAgcdhqg0xAEjv01hO4ns/xxlothgiJ6x4BYPrIpWxFAqwxWLphlMUoY1LYp3mN6qF2EUxKgakVarvgXXsyfIgrl8HIzKT2cLHMPUzSLoV2Oc3bKBlA9GpaRedZNaUJ2aIkd6jGE+VG3OAg0EpDkaA+6rGkaw20SUYgjlNSWe5RQbMOgHWA3YhglpestWzWro4N6fOaNOzaGw9rKZlkqXaoJsrMkxXs0RSl+K83LZdVJy1Co+DS//FkCeoiEiCYsRVxtIP4jibIoA4lrtZpiU+TRDx5UTvL62KUPpaoS5yHserVj4QTCtALcd+/vAaesmjBy2YHfIzbl2uxQjU/D83DSnXBAuowGA5aaCFKkkixy0nMWsc7ZWT+gY/1Pw5pqn9LulmY4whTD4JJf4CG/Ug/GZTelKLJx8Je/eKIdYridfg7ZHvxn0U0XVG4Iai/3dxutY3UIiKTWeGRkU8/QzNWrsvMkYxHIK3PeUZ83Bhims25L9vpmr8jlFZ2QxUEY68N5EvE5b/Ta842rc70473WHS+CFLMx0J0H70E6mzqY3ms2ThTRh6MAbmyZqagosFgp44mgqeIIr8ZNlMsyt/vpN3/hdNTjom/WbL7aHh5XiYx6Yo4FeCOZ+l/MuF++fTC3kFGEVC2kQNJ19n2aaZVpL+9Ww/bjFNie5ZcX+Znz9yW2DaXysSGeLC+R86fr98kX6pSsjPLwyrUv+Srf8Bffo++CyXPD2iyRtBrQ+P5ux31PQfM1KiVY/knEGNd36/9EWXwPWCQhhlJV8r+HkhH8obTXDNmpdO2HYjWOGVRs9wdc3jAX8RMbFh9csKSWgpMyRR18X9Uv2bI/tlm3asX0Tq6RLVvHgllb3RFj8JlpdV2aLo2vlk76kfHRhMuUbuN0+VyofHqeKRTf7te1JQ87oP2sczRU1FPwx6MW+zVx0x5391tZEP6SYQN/Fmd7h/Ik/MtgckhUnW69o1jcKqLX92C6sIZg+L5mUek7bUF8HxLL7wnLltRmEXlhGusC1nsOFNXoRu8tnoqfuwLN5uT3nR7yrOIB4BwKEBXR1qTMphVOd+8glURjGfimYuvE2UfV10i/2gbxn0/psMF5EQ9zXn/A6G85nQiTsVuP1RovIDkRshdIqVX4/8kMges5MmS/o4G3SHPJUdwpVmif2li2YTYBpoZcCzcY7sdV6Cqf2/1kxhTvc/p2Sec1t5LjoktrNh9RGkdNOEpKXWQpgmmL7iwLznuwq6jH+w8yy8CRLbHT25BA4ChMXnWTgfv7PEnzvSXXCEhRAJrqXm3jwqjMJPv7c1OUFp6dZXd8f8tVTrdp8bXX+dXDliP+Jn+rEN36+6bl3Swv5j6aFeEuLc4n+7bQQlKtyKn84Ldjw48O2sj7/7v0P","w":190,"h":290,"aspect":"fixed","title":"PM: Super Activity Example"},{"xml":"dVLBbsMgDP0ajq0g0CTXJVt72mU97EwTN7CSUBG2tH8/Q8jaSisSkv387GdsCK/7y87Js3q3LRjC3wivnbV+tvpLDcaQjOqW8FeSZRQvybZPoixG6Vk6GPw/CfbwBY1HhpGHIBYIL87rowxoTO0/rIE5Ih8ii0YW3Ef50V+XHGe/hxYClRJeTUp72J9lE6ITvhIx5fugzNAclWztlLidkeOY7NE7e4JP3XqFCMpVR21MbY11UYZv4wm4HfwdTuNBXBrdDYg1OAhwibhPfQr04wQq2Zy62PJSY7ADMqrG9rpJzczP/wHn4fJ0zOxuKjuwPXh3RUpKWIn1RhSi4BtalrnIBYMV43OVayqwZjwv85IXRcZyllN+o0xpEIFG54VSBbpTSVwkTI6z3/01cFs9Gmlfi5s+w+LePl2kPvzJXw==","w":100,"h":40,"aspect":"fixed","title":"PM: Artifact"},{"xml":"dZLNbsMgDMefhjsNe4JkS0+7rIedKXEDK+AI2JK+/QwhayOtSJHsv3+OP4CJzi3HICf9jgNYJt6Y6AJiWi23dGAta7gZmHhlTcPpY03/JHooUT7JAD79k4DnL1CJCCvPuVgGegxOWhNdzXUfaGENXfahrUqT3X0DMd22pIDffoCMcibaWZsEp0mqHJ1pTtJ0crn2gcyo5YBzZUcrY6x2TAGv8GmGpEmhcu3FWNuhxVDKiL6crKNPDzovh3RqfPSkKVoFhAqeap8ZKDtopbqOpeXtHx49Ea1CZ1Ql1/F/ICRYni768LCVI6CDFG6EzHWETPD1MrgGM+qa9lI1GVd//Eu9XxsZddObWy9yc+8PpqC79/QL","w":100,"h":40,"aspect":"fixed","title":"FTG: Formalism"},{"xml":"dVLbbqMwEP0apN03wG3CPhYCKWlCmoaEwktlwOFmMGvM9evXQLJtpBYJ6ZzxmfF45ghAy7s1hWW8IyHCAtAFoFFC2IzyTkMYC7KYhAJYCbIs8l+QjR9OpelULCFFBfsmgfgpChhXYOiPl42CHSxqOBaxKSyqC6E5ZAkprpXyN4LRLMxh8cG+0dwul0d631fF+lt2FcNyhBVDRZDgX6ln+KvKjHT9/N4OvENNy4bzxXla4lO2sUy1BL6o/M2zTlUt7bEw+niZHUCalV6i7Y6V2cRb3S6Rvhdk9bzN2gocOJLaIHBPMX6UgUdMQ10qG7LxIjEI6qbbLFZZq4AHFTShvji+PL8tyUqJTDuEdb+v+dBlFeF0kHTLpu62JpbhQpP9gcbTi9ddQoe3+drnEmoOkrtwbBI+N/5rpa7PkFnloBDHPKW8iLF3SqV1+Wt/C0Bt44ShYwmD8fktXzWPxSwfxy9xeEkw1ggmdJoT0KaPxymqkgH60/zEkRMG2Rc+D79BlKHux+1LX3ayRiRHjPZc0iYhi2fFw2wQMUZJFLP7GKxmHv3P/LQSB9c13+jVXDf6aeJJeufxfw==","w":40,"h":40,"aspect":"fixed","title":"FTG: Manual Transformation"},{"xml":"dVLLcqMwEPwaqnZvgBKbPQIGB8fBcYxN4JISICNAIBbE8+tXGHsTVyUHVXWPekajmRaAnvfrCpb4hUaICMAQgF5RymaU9zoiRJDFJBLASpBlkR9BNn+4lS63YgkrVLBvEmiQopBxBYHB9NgkUBtGc8hQxMNOBYv6TCvOE1pci+VvlKBZC7n2g30jujUgT/S+t5oNt/Qaw3KCNUNFmJBfqW8Gq9qKDeP03o28S13PxtPZVZfkmG1sSytBICp/86zXNFt/LMwBL7M9SLPST/SXQ221eGs4JTJ2gqydtllXgz1HUheG3hGTRxn41DK1pbKhGz8Ww7Bp+81ilXUKeNBAGxmLw/PT25KulNhyItgMu4YPXtYQSUfJsJ3K2zbUNj1osT/QVJ/9/hy5vM3XIZdQu5e8hevQ6KkNXmttfYLMLkeFutYx5UXMnVsqncd/+1sAWocThg4lDKfvd3zdPIZZPq1A4rBCdTLC4DInceKUQfaFnxNCdEpodZkjMEUViCqPz8NvUcVQ/6MDpC87WSOaI1YNXNIlEcOz4mE2iYhREmN2H4P1zOP/mZ924uC65hu9GuxGP418kd75/B8=","w":40,"h":40,"aspect":"fixed","title":"FTG: Automated Transformation"}]</mxlibrary>';
 
-  "data_flow": "endArrow=classic;html=1;strokeWidth=1;fontSize=14;fontColor=#000000;dashed=1;",
+  // Styles for our different edge types
+  const styles = {
+    control_flow: "endArrow=classic;html=1;strokeWidth=2;",
+    data_flow: "endArrow=classic;html=1;strokeWidth=1;fontSize=14;fontColor=#000000;dashed=1;",
+    detail: "endArrow=classic;html=1;dashed=1;strokeWidth=2;curved=1;fontColor=#000000;endFill=0;endSize=10;startSize=8;",
+    typed_by: "endArrow=blockThin;html=1;strokeWidth=1;fontSize=14;fontColor=#000000;dashed=1;dashPattern=1 1;strokeColor=#666666;endFill=1;endSize=6;",
+    produce_consume: "endArrow=classic;html=1;",
+  };
 
-  "detail": "endArrow=classic;html=1;dashed=1;strokeWidth=2;curved=1;fontColor=#000000;endFill=0;endSize=10;startSize=8;",
 
-  "typed_by": "endArrow=blockThin;html=1;strokeWidth=1;fontSize=14;fontColor=#000000;dashed=1;dashPattern=1 1;strokeColor=#666666;endFill=1;endSize=6;",
+  function checkEdge(ui, edge) {
 
-  "produce_consume": "endArrow=classic;html=1;",
-};
+    function setStyle(style_type) {
+      edge.setAttribute("pmRole", style_type)
+      ui.editor.graph.setCellStyle(styles[style_type], [edge]);
+      ui.editor.graph.view.clear(edge, true, false);
+      ui.editor.graph.view.createState(edge);
+      ui.editor.graph.view.revalidate();
+    }
 
+    sourceCell = edge.source;
+    targetCell = edge.getTerminal();
 
-function checkEdge(ui, edge) {
-
-  function setStyle(style_type) {
-    edge.setAttribute("pmRole", style_type)
-    ui.editor.graph.setCellStyle(styles[style_type], [edge]);
-    ui.editor.graph.view.clear(edge, true, false);
-    ui.editor.graph.view.createState(edge);
-    ui.editor.graph.view.revalidate();
-  }
-
-  sourceCell = edge.source;
-  targetCell = edge.getTerminal();
-
-  if (!sourceCell || !targetCell) {
-    return;
-  }
-
-  sourceType = sourceCell.getAttribute("pmRole");
-  targetType = targetCell.getAttribute("pmRole");
-
-  function isActivity(type) {
-    return type === "man_activity" || type === "autom_activity";
-  }
-  function isTransformation(type) {
-    return type === "man_transformation" || type === "auto_transformation";
-  }
-  function isControlNode(type) {
-    return isActivity(type) || type === "initial" || type === "final" || type === "fork_join" || type === "decision";
-  }
-
-  if (isControlNode(sourceType)) {
-    if (isControlNode(targetType)) {
-      setStyle("control_flow")
+    if (!sourceCell || !targetCell) {
       return;
     }
-  }
 
-  if (isActivity(sourceType)) {
-    if (isTransformation(targetType)) {
-      setStyle("typed_by")
-      return;
-    }
-    else if (targetType === "super_activity") {
-      setStyle("detail")
-      return;
-    }
-    else if (targetType === "artifact") {
-      // console.log("PM data-flow (produce) link")
-      setStyle("data_flow")
-    }
-  }
+    sourceType = sourceCell.getAttribute("pmRole");
+    targetType = targetCell.getAttribute("pmRole");
 
-  if (sourceType === "artifact") {
-    if (targetType === "formalism") {
-      setStyle("typed_by")
-      return;
+    function isActivity(type) {
+      return type === "man_activity" || type === "autom_activity";
     }
-    else if (isActivity(targetType)) {
-      // console.log("PM data-flow (consume) link")
-      setStyle("data_flow")
-      return;
+    function isTransformation(type) {
+      return type === "man_transformation" || type === "auto_transformation";
     }
-  }
+    function isControlNode(type) {
+      return isActivity(type) || type === "initial" || type === "final" || type === "fork_join" || type === "decision";
+    }
 
-  if (isTransformation(sourceType)) {
-    if (targetType === "formalism") {
-      // console.log("FTG produce link")
-      setStyle("produce_consume")
-      return;
-    }
-  }
-
-  if (sourceType === "formalism") {
-    if (isTransformation(targetType)) {
-      // console.log("FTG consume link")
-      setStyle("produce_consume")
-      return;
-    }
-  }
-}
-
-Draw.loadPlugin(function(ui) {
-  ui.editor.graph.model.addListener(mxEvent.CHANGE, (_, eventObj) => {
-    eventObj.properties.edit.changes.forEach(change => {
-      if (change.constructor.name === "mxTerminalChange") {
-        const edge = change.cell;
-        checkEdge(ui, edge);
-        console.log(edge);
+    if (isControlNode(sourceType)) {
+      if (isControlNode(targetType)) {
+        setStyle("control_flow")
+        return;
       }
-    })
-    // const {
-    //   edge, // mxCell
-    //   source, // bool: source or target changed
-    //   terminal, // mxCell connected to, or undefined
-    //   previous, // mxCell previously connected to, or undefined
-    // } = eventObj.properties;
-  });
-})
+    }
+
+    if (isActivity(sourceType)) {
+      if (isTransformation(targetType)) {
+        setStyle("typed_by")
+        return;
+      }
+      else if (targetType === "super_activity") {
+        setStyle("detail")
+        return;
+      }
+      else if (targetType === "artifact") {
+        // console.log("PM data-flow (produce) link")
+        setStyle("data_flow")
+      }
+    }
+
+    if (sourceType === "artifact") {
+      if (targetType === "formalism") {
+        setStyle("typed_by")
+        return;
+      }
+      else if (isActivity(targetType)) {
+        // console.log("PM data-flow (consume) link")
+        setStyle("data_flow")
+        return;
+      }
+    }
+
+    if (isTransformation(sourceType)) {
+      if (targetType === "formalism") {
+        // console.log("FTG produce link")
+        setStyle("produce_consume")
+        return;
+      }
+    }
+
+    if (sourceType === "formalism") {
+      if (isTransformation(targetType)) {
+        // console.log("FTG consume link")
+        setStyle("produce_consume")
+        return;
+      }
+    }
+  }
+
+  Draw.loadPlugin(function(ui) {
+    ui.editor.graph.model.addListener(mxEvent.CHANGE, (_, eventObj) => {
+      eventObj.properties.edit.changes.forEach(change => {
+        if (change.constructor.name === "mxTerminalChange") {
+          const edge = change.cell;
+          checkEdge(ui, edge);
+          // console.log(edge);
+        }
+      })
+    });
+    const library = new LocalLibrary(ui, libraryData, "FTG+PM");
+    ui.loadLibrary(library);
+  })
+}
